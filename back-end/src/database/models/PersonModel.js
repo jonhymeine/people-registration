82cb0fc3bb2bbed86066ec9da5fdf588946c822d
Jonathas Meine <jonhy.meine@gmail.com>
2024-05-29 12:55:39 -0300

    feat: add sequelize validation to Person attributes and disable timestamps

--- back-end/src/database/models/PersonModel.js
+++ back-end/src/database/models/PersonModel.js
@@ -1,34 +1,55 @@
 const sequelize = require('../connection.js');
 const {DataTypes} = require('sequelize');
 
-const Person = sequelize.define('Person', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+const Person = sequelize.define(
+    'Person',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        cpf: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        rg: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        birthdate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        gender: {
+            type: DataTypes.CHAR(1),
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
     },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    cpf: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    rg: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    birthdate: {
-        type: DataTypes.DATE,
-        allowNull: false,
-    },
-    gender: {
-        type: DataTypes.CHAR(1),
-        allowNull: false,
-    },
-});
+    {
+        timestamps: false,
+    }
+);
 
 module.exports = Person;
