273fcf287802aeb221308f30979c02e929488b99
Jonathas Meine <jonhy.meine@gmail.com>
2024-05-29 12:54:07 -0300

    chore: split the Postgres connection URI properties

--- back-end/src/database/connection.js
+++ back-end/src/database/connection.js
@@ -1,8 +1,9 @@
 const {Sequelize} = require('sequelize');
 
 require('dotenv').config();
-const databaseUrl = process.env.DATABASE_URL;
+const {DATABASE_USER, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_HOST, DATABASE_NAME, DATABASE_SCHEMA} = process.env;
+const databaseUri = `postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}?schema=${DATABASE_SCHEMA}`;
 
-const sequelize = new Sequelize(databaseUrl);
+const sequelize = new Sequelize(databaseUri);
 
 module.exports = sequelize;
