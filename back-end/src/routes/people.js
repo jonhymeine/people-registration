6bd39914f8fa9234b4d402ad4bff20b57de49033
Jonathas Meine <jonhy.meine@gmail.com>
2024-05-30 22:47:37 -0300

    chore: change fetch by filtering function and routing

--- back-end/src/routes/people.js
+++ back-end/src/routes/people.js
@@ -5,7 +5,7 @@ const PeopleController = require('../controllers/PeopleController');
 const peopleController = new PeopleController();
 
 router.get('/people', (req, res) => peopleController.getPeople(req, res));
-router.get('/person', (req, res) => peopleController.getPerson(req, res));
+router.get('/person/:searchMethod/:searchValue', (req, res) => peopleController.getPeopleByFilter(req, res));
 router.post('/person', (req, res) => peopleController.createPerson(req, res));
 router.put('/person', (req, res) => peopleController.updatePerson(req, res));
 router.delete('/person', (req, res) => peopleController.deletePerson(req, res));
